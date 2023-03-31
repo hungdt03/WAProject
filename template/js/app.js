ba319f346047d1dcdbf0ec5dd24307e394cbab51
Thanh <hungktpm1406@gmail.com>
2023-03-31 16:18:51 +0700

    update heading product

--- template/js/app.js
+++ template/js/app.js
@@ -10,10 +10,12 @@ function changeHeader(pixels) {
         if(nav) {
             nav.backgroundColor = '#00483d';
             header.innerHTML = nav.innerHTML;
+            header.classList.add('fixed-scroll-header')
         }
         
     } else {
         const getHeader = localStorage.getItem('header');
+        header.classList.remove('fixed-scroll-header')
         if(getHeader) {
             header.innerHTML = JSON.parse(getHeader);
         }
@@ -23,3 +25,28 @@ function changeHeader(pixels) {
 window.onscroll = function(e) {
     changeHeader(170)
 }
+
+
+
+// Xử lý carousel cho product sale
+const mainProducts = document.querySelector('.salesflash__content-wrapper');
+const wrapperProducts = document.querySelector('.salesflash__content-inner')
+const allProducts = document.querySelectorAll('.salesflash__content-item');
+const widthProduct = allProducts[0].clientWidth;
+const btnPrev = document.querySelector('.salesflash__content-wrapper-button .btn-prev')
+const btnNext = document.querySelector('.salesflash__content-wrapper-button .btn-next')
+let currentIndex = 0;
+
+btnPrev.onclick = function(e) {
+    if(currentIndex == 0)
+        return
+    currentIndex += widthProduct
+    wrapperProducts.style = `transform: translateX(${currentIndex}px)`;
+}
+
+btnNext.onclick = function(e) {
+    if(currentIndex == -widthProduct * (allProducts.length - 4))
+        return
+    currentIndex -= widthProduct
+    wrapperProducts.style = `transform: translateX(${currentIndex}px)`;
+}   
